14cd8ef39f4f7d9b41c69ed8c24624a15c6a5926
marinelife <ghdic77@gmail.com>
2023-02-08 19:37:06 +0900

    프로젝트 생성 및 application.properties 기본 세팅

---
+++ src/main/java/com/developlife/reviewtwits/ReviewtwitsApplication.java
@@ -0,0 +1,13 @@
+package com.developlife.reviewtwits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewtwitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewtwitsApplication.class, args);
+	}
+
+}
