f6dcc5277c250940e039967a93b6265bcf45f54f
WhalesBob <ndw8200@naver.com>
2023-02-15 02:50:19 +0900

    #4 [ADD] Github Action 의 build 테스트를 위한, 일부러 실패하는 테스트 만들기

---
+++ src/test/java/com/developlife/reviewtwits/MockTests.java
@@ -0,0 +1,16 @@
+package com.developlife.reviewtwits;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MockTests {
+
+    @DisplayName("일부러 실패하고, Github Action 의 gradlew 가 성공하는지 확인하는 테스트")
+    @Test
+    void mockTestForCICD(){
+        int x = 1;
+        assertThat(x).isEqualTo(0);
+    }
+}
