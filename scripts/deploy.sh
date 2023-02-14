356545ac581493e8b680ae625d36d56affedbedd
Nam Dongwoo <96509257+WhalesBob@users.noreply.github.com>
2023-02-15 01:18:50 +0900

    #4 [FIX] Github Action 동작 이후 배포를 위한 시도

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,31 +1,36 @@
-#!/usr/bin/env bash
+#!/bin/bash
+PROJECT_NAME="github_action"
+JAR_PATH="/home/ubuntu/github_action/build/libs/*.jar"
+DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
+DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
+DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
+APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
+BUILD_JAR=$(ls $JAR_PATH)
+JAR_NAME=$(basename $BUILD_JAR)
 
-REPOSITORY=/home/ubuntu/app
+echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
+echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
+cp $BUILD_JAR $DEPLOY_PATH
 
-CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
-
-if [ -z "$CURRENT_PID" ]; then
-  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 동작중인 어플리케이션 존재 X" >> $DEPLOY_LOG_PATH
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+  echo "> 현재 동작중인 어플리케이션 존재 O" >> $DEPLOY_LOG_PATH
+  echo "> 현재 동작중인 어플리케이션 강제 종료 진행" >> $DEPLOY_LOG_PATH
+  echo "> kill -9 $CURRENT_PID" >> $DEPLOY_LOG_PATH
+  kill -9 $CURRENT_PID
 fi
 
-echo "> 새 애플리케이션 배포"
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
+nohup java -jar -Dspring.profiles.active=local $DEPLOY_JAR --server.port=8081 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
 
-JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
+sleep 3
 
-echo "> JAR NAME: $JAR_NAME"
-
-echo "> $JAR_NAME 에 실행권한 추가"
-
-chmod +x $JAR_NAME
-
-echo "> $JAR_NAME 실행"
-
-nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+echo "> 배포 종료 : $(date +%c)" >> $DEPLOY_LOG_PATH
