880beb67253da8ff2f3591bd77da0403f614ecae
WhalesBob <ndw8200@naver.com>
2023-02-19 17:41:19 +0900

    #11 [FEAT] 기본 파일 업로드 시 체크할 테스트 케이스 설명 추가

---
+++ src/test/java/com/developlife/reviewtwits/file/BasicFileUploadTest.java
@@ -0,0 +1,25 @@
+package com.developlife.reviewtwits.file;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BasicFileUploadTest {
+
+    @Test
+    @DisplayName("파일 업로드 시, 해당 경로, 같은 이름으로 파일이 존재하는지 확안하기.")
+    void checkFileUpload_existInDirectory(){
+
+    }
+
+    @Test
+    @DisplayName("파일 업로드 시, 해당 경로, 같은 이름으로 FileStorage 테이블에 정보가 업데이트되는지 확안하기")
+    void checkFileUpload_existInFileStorageTable(){
+
+    }
+
+    @Test
+    @DisplayName("파일 업로드 시, 정확한 정보가 FileManager 테이블에 업데이트되는지 확인하기")
+    void checkFileUpload_existInFileManagerTable(){
+
+    }
+}
