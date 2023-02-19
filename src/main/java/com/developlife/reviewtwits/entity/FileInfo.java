3a3b84a0047fbd7fe62143006ce9fa5331b76672
WhalesBob <ndw8200@naver.com>
2023-02-20 01:50:07 +0900

    #11 [REFACTOR] FileStorage 에서 FileInfo 로 Entity 이름 변경

---
+++ src/main/java/com/developlife/reviewtwits/entity/FileInfo.java
@@ -0,0 +1,34 @@
+package com.developlife.reviewtwits.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Table(name = "FileStorage")
+public class FileInfo {
+
+    @Id
+    @Column(name = "file_storage_id")
+    @GeneratedValue
+    private Long fileID;
+
+    @Column(name = "file_path")
+    private String filePath;
+
+    @Column(name = "real_filename")
+    private String realFilename;
+
+    @Column(name = "original_filename")
+    private String originalFilename;
+
+    public FileInfo() {
+    }
+
+    public FileInfo(String filePath, String realFilename, String originalFilename) {
+        this.filePath = filePath;
+        this.realFilename = realFilename;
+        this.originalFilename = originalFilename;
+    }
+}
