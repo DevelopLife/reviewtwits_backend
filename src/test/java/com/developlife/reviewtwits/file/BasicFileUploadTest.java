7b9015b35ce5498f80f200ac08f730c93f4dec4c
WhalesBob <ndw8200@naver.com>
2023-02-19 19:40:40 +0900

    #11 [FEAT] 기본 파일 업로드 검증을 위한 테스트 코드 작성

--- src/test/java/com/developlife/reviewtwits/file/BasicFileUploadTest.java
+++ src/test/java/com/developlife/reviewtwits/file/BasicFileUploadTest.java
@@ -1,14 +1,56 @@
 package com.developlife.reviewtwits.file;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
+import java.io.IOException;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BasicFileUploadTest {
 
-    @Test
-    @DisplayName("파일 업로드 시, 해당 경로, 같은 이름으로 파일이 존재하는지 확안하기.")
-    void checkFileUpload_existInDirectory(){
+    @Autowired
+    private TestRestTemplate restTemplate;
 
+    @Test
+    @DisplayName("파일 업로드 시, 같은 내용으로 파일이 존재하는지 확안하기.")
+    void checkFileUpload_existInDirectory() throws IOException {
+
+        Path path = Files.createTempFile("test",".txt");
+        Files.write(path, "test file content".getBytes());
+
+        String expectedContent = "test file content";
+        long expectedSize = Files.size(path);
+
+        // MultiValueMap 생성
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", new FileSystemResource(path));
+
+        // 요청 보내기
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity("/files/save", requestEntity, String.class);
+
+        // 검증
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.ACCEPTED);
+        assertThat(response.getHeaders().getContentLength()).isEqualTo(expectedSize);
+
+        String uploadedContent = new String(Files.readAllBytes(Paths.get("uploaded-file-path")));
+        assertThat(uploadedContent).isEqualTo(expectedContent);
     }
 
     @Test
