3cc7801a88c5ba9f4d26a29d646440cfb032896f
WhalesBob <ndw8200@naver.com>
2023-02-19 21:07:14 +0900

    #11 [FEAT] 기본 파일 저장(without DB) 테스트 2개로 증가

--- src/test/java/com/developlife/reviewtwits/file/BasicFileUploadTest.java
+++ src/test/java/com/developlife/reviewtwits/file/BasicFileUploadTest.java
@@ -2,6 +2,7 @@ package com.developlife.reviewtwits.file;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +16,6 @@ import java.io.IOException;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,33 +25,46 @@ public class BasicFileUploadTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Test
-    @DisplayName("파일 업로드 시, 같은 내용으로 파일이 존재하는지 확안하기.")
-    void checkFileUpload_existInDirectory() throws IOException {
+    @Nested
+    class CheckFileUploadTest{
 
-        Path path = Files.createTempFile("test",".txt");
-        Files.write(path, "test file content".getBytes());
+        @Test
+        @DisplayName("파일 업로드 시, 같은 내용으로 파일이 존재하는지 확안하기.")
+        void checkFileUpload_existInDirectory_case1() throws IOException {
+            checkFileUpload_existInDirectory("spring test files");
+        }
 
-        String expectedContent = "test file content";
-        long expectedSize = Files.size(path);
+        @Test
+        @DisplayName("파일 업로드 시, 같은 내용으로 파일이 존재하는지 확안하기.")
+        void checkFileUpload_existInDirectory_case2() throws IOException {
+            checkFileUpload_existInDirectory("hello file upload");
+        }
 
-        // MultiValueMap 생성
-        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-        body.add("file", new FileSystemResource(path));
 
-        // 요청 보내기
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-        ResponseEntity<String> response = restTemplate.postForEntity("/files/save", requestEntity, String.class);
+        void checkFileUpload_existInDirectory(String inputContent) throws IOException{
+            Path path = Files.createTempFile("test",".txt");
+            Files.write(path, inputContent.getBytes());
 
-        // 검증
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.ACCEPTED);
-        assertThat(response.getHeaders().getContentLength()).isEqualTo(expectedSize);
+            // MultiValueMap 생성
+            MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+            body.add("attachedFiles", new FileSystemResource(path));
 
-        String uploadedContent = new String(Files.readAllBytes(Paths.get("uploaded-file-path")));
-        assertThat(uploadedContent).isEqualTo(expectedContent);
+            // 요청 보내기
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+            HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+            ResponseEntity<String> response = restTemplate.postForEntity("/files/save", requestEntity, String.class);
+
+            // 검증
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.ACCEPTED);
+
+            String storedPath = response.getBody().substring(10,response.getBody().length()-1);
+            String uploadedContent = new String(Files.readAllBytes(Path.of(storedPath)));
+            assertThat(uploadedContent).isEqualTo(inputContent);
+        }
     }
+
+
 
     @Test
     @DisplayName("파일 업로드 시, 해당 경로, 같은 이름으로 FileStorage 테이블에 정보가 업데이트되는지 확안하기")
