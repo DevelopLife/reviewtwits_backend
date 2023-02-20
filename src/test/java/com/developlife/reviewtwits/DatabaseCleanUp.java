20adf4505236f38fcedfb7c674d3f2b840c8be49
marinelife <ghdic77@gmail.com>
2023-02-20 18:55:34 +0900

    #7 [FIX] h2쿼리를 mariadb쿼리로 대체

--- src/test/java/com/developlife/reviewtwits/DatabaseCleanUp.java
+++ src/test/java/com/developlife/reviewtwits/DatabaseCleanUp.java
@@ -51,14 +51,14 @@ public class DatabaseCleanUp implements InitializingBean {
     @Transactional
     public void execute() {
         entityManager.flush();
-        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+        entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 0").executeUpdate();
 
         for (final String tableName : tableNames) {
             entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
-            entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1").executeUpdate();
+            entityManager.createNativeQuery("ALTER TABLE " + tableName + " AUTO_INCREMENT = 1").executeUpdate();
         }
 
-        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+        entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 1").executeUpdate();
     }
 
 }
