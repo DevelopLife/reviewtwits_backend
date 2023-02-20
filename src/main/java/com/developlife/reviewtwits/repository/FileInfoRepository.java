d563ad4f539ca2ca4aa09f1c04208edc461b9381
WhalesBob <ndw8200@naver.com>
2023-02-20 14:58:44 +0900

    #11 [FEAT] Repository 에, 테스트를 위한 findBy 메소드 추가

--- src/main/java/com/developlife/reviewtwits/repository/FileInfoRepository.java
+++ src/main/java/com/developlife/reviewtwits/repository/FileInfoRepository.java
@@ -4,8 +4,11 @@ import com.developlife.reviewtwits.entity.FileInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface FileInfoRepository extends JpaRepository<FileInfo, Long> {
 
+    Optional<FileInfo> findByOriginalFilename(String originalFilename);
 }
--- src/main/java/com/developlife/reviewtwits/repository/FileManagerRepository.java
+++ src/main/java/com/developlife/reviewtwits/repository/FileManagerRepository.java
@@ -2,7 +2,11 @@ package com.developlife.reviewtwits.repository;
 
 import com.developlife.reviewtwits.entity.FileManager;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface FileManagerRepository extends JpaRepository<FileManager, Long> {
-
+    Optional<FileManager> findByFileStorageID(Long fileStorageId);
 }
