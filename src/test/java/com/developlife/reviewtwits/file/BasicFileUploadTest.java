7fc577cd324576cb50226675ba85a761efdfa2b9
Nam Dongwoo <96509257+WhalesBob@users.noreply.github.com>
2023-02-22 16:23:19 +0900

    [FIX] 어떤 에러가 나오는지 점검하기(400 에러)

--- src/test/java/com/developlife/reviewtwits/file/BasicFileUploadTest.java
+++ src/test/java/com/developlife/reviewtwits/file/BasicFileUploadTest.java
@@ -35,7 +35,7 @@ public class BasicFileUploadTest extends FileUploadTest {
             ResponseEntity<String> response = fileUpload(inputContent,filename,suffix, 12L, "Test");
             // 검증
             System.out.println("response.getStatusCode = " +  response.getStatusCode());
-            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
 
             String storedPath = getStoredFullPath(response);
             String uploadedContent = new String(Files.readAllBytes(Path.of(storedPath)));
