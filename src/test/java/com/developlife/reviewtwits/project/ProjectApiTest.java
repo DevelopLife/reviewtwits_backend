fb5764204ba8432588c71356d290cb8034db23aa
marinelife <ghdic77@gmail.com>
2023-03-09 14:58:39 +0900

    #41 [FEAT] project 시작

---
+++ src/test/java/com/developlife/reviewtwits/project/ProjectApiTest.java
@@ -0,0 +1,10 @@
+package com.developlife.reviewtwits.project;
+
+import com.developlife.reviewtwits.ApiTest;
+
+/**
+ * @author ghdic
+ * @since 2023/03/09
+ */
+public class ProjectApiTest extends ApiTest {
+}
