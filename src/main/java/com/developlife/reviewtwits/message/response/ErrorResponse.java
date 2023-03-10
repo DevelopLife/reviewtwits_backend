db254489024474ff9626d75d491985180b577f53
WhalesBob <ndw8200@naver.com>
2023-03-10 16:57:01 +0900

    DevelopLife#13 [CHORE] ErrorResponse 관련 Builder annotation 위치 변경

--- src/main/java/com/developlife/reviewtwits/message/response/ErrorResponse.java
+++ src/main/java/com/developlife/reviewtwits/message/response/ErrorResponse.java
@@ -6,8 +6,9 @@ import lombok.Builder;
  * @author ghdic
  * @since 2023/03/02
  */
-@Builder
+
 public record ErrorResponse(String message, String errorType, String fieldName) {
+    @Builder
     public ErrorResponse {
     }
 }
