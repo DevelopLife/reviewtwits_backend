2cbae85a4c726b72b9c88a166300d0aaf537d5de
WhalesBob <ndw8200@naver.com>
2023-03-10 17:22:30 +0900

    DevelopLife#13 [FEAT] 이미지 불러오기 요청 기능 추가

--- src/main/java/com/developlife/reviewtwits/controller/FileController.java
+++ src/main/java/com/developlife/reviewtwits/controller/FileController.java
@@ -2,18 +2,24 @@ package com.developlife.reviewtwits.controller;
 
 import com.developlife.reviewtwits.entity.FileInfo;
 import com.developlife.reviewtwits.message.request.FileUpdateRequest;
+import com.developlife.reviewtwits.message.response.ErrorResponse;
 import com.developlife.reviewtwits.service.FileStoreService;
 import com.developlife.reviewtwits.type.FileReferenceType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.List;
+
+import static com.developlife.reviewtwits.handler.ExceptionHandlerTool.makeErrorResponse;
 
 @Slf4j
 @RestController
@@ -39,8 +45,22 @@ public class FileController {
         return ResponseEntity.accepted().body(resource.toString());
     }
 
+    @GetMapping("/request-images/{fileName}")
+    public Resource downloadImage(@PathVariable String fileName) throws MalformedURLException {
+        if(FileReferenceType.isValidFileType("image",fileName)){
+            return new UrlResource("file:"+ fileStore.getFullPath(fileName));
+        }
+        throw new MalformedURLException();
+    }
+
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<String> attachedFileNullExceptionHandler(){
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MalformedURLException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public List<ErrorResponse> malformedUrlExceptionHandler(){
+        return makeErrorResponse(new FileNotFoundException(), "fileName");
     }
 }
