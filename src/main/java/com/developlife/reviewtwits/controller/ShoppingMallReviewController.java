a7b8fabeed2c1da743ae734c036a115802a8ec39
WhalesBob <ndw8200@naver.com>
2023-03-16 18:33:39 +0900

    #59 [FEAT] 쇼핑몰 리뷰 정보를 전달하기 위한 Controller 구현

--- src/main/java/com/developlife/reviewtwits/controller/ShoppingMallReviewController.java
+++ src/main/java/com/developlife/reviewtwits/controller/ShoppingMallReviewController.java
@@ -1,7 +1,10 @@
 package com.developlife.reviewtwits.controller;
 
 
+import com.developlife.reviewtwits.exception.product.ProductNotFoundException;
+import com.developlife.reviewtwits.message.request.review.ReviewProductURLRequest;
 import com.developlife.reviewtwits.message.request.review.ShoppingMallReviewWriteRequest;
+import com.developlife.reviewtwits.message.response.review.ShoppingMallReviewProductResponse;
 import com.developlife.reviewtwits.service.ShoppingMallReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,4 +31,13 @@ public class ShoppingMallReviewController {
         reviewService.saveShoppingMallReview(request);
     }
 
+    @GetMapping(value = "/shopping", produces = "application/json")
+    public ShoppingMallReviewProductResponse getShoppingMallReviewInfo(@Valid @RequestBody ReviewProductURLRequest request){
+        ShoppingMallReviewProductResponse result =  reviewService.findShoppingMallReviewTotalInfo(request.productURL());
+
+        if(result == null){
+            throw new ProductNotFoundException("입력한 URL 로 등록된 제품이 존재하지 않습니다");
+        }
+        return result;
+    }
 }
