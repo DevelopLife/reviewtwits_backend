cd803e03ccba2d53c2d314e5801d51743fa52f70
WhalesBob <ndw8200@naver.com>
2023-03-16 17:46:51 +0900

    #60 [FEAT] Review Entity 와 해당 Repository 생성

---
+++ src/main/java/com/developlife/reviewtwits/entity/Review.java
@@ -0,0 +1,47 @@
+package com.developlife.reviewtwits.entity;
+
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * @author WhalesBob
+ * @since 2023-03-13
+ */
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "review_id")
+    private long reviewId;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "projectId")
+    private Project project;
+
+    @Column(name = "certification_flag")
+    @ColumnDefault(value = "false")
+    private boolean certificationFlag;
+
+    private String content;
+
+    @Column(name = "product_url")
+    private String productUrl;
+
+    private int score;
+
+    @Transient
+    @Setter
+    private List<String> reviewImageNameList;
+}
---
+++ src/main/java/com/developlife/reviewtwits/repository/ReviewRepository.java
@@ -0,0 +1,13 @@
+package com.developlife.reviewtwits.repository;
+
+import com.developlife.reviewtwits.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+}
+
