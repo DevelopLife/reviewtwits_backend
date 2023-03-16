f4504fb887ac3f49108407445dcfce5485dbeb8a
WhalesBob <ndw8200@naver.com>
2023-03-16 18:31:32 +0900

    #59 [FEAT] ProductID 가 존재하지 않는 경우를 커버하는 exception 및 handler 추가

---
+++ src/main/java/com/developlife/reviewtwits/handler/ProductExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.developlife.reviewtwits.handler;
+
+import com.developlife.reviewtwits.exception.product.ProductNotFoundException;
+import com.developlife.reviewtwits.exception.project.ProjectIdNotFoundException;
+import com.developlife.reviewtwits.message.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+import static com.developlife.reviewtwits.handler.ExceptionHandlerTool.makeErrorResponse;
+
+/**
+ * @author WhalesBob
+ * @since 2023-03-15
+ */
+public class ProductExceptionHandler {
+    @ExceptionHandler(ProductNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public List<ErrorResponse> projectIdNotFoundExceptionHandler(ProjectIdNotFoundException e){
+        return makeErrorResponse(e, "productURL");
+    }
+}
