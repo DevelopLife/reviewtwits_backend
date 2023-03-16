65e8725502bcdc4c0904c0e0f95d7f1cf823bb09
WhalesBob <ndw8200@naver.com>
2023-03-16 17:39:47 +0900

    #60 [FEAT] Product Entity 및 관련 Repository 생성

---
+++ src/main/java/com/developlife/reviewtwits/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.developlife.reviewtwits.repository;
+
+import com.developlife.reviewtwits.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * @author WhalesBob
+ * @since 2023-03-13
+ */
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findProductByProductUrl(String productUrl);
+
+}
