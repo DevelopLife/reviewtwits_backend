6df964e64e236784b69e7512edf6b06964633e7b
WhalesBob <ndw8200@naver.com>
2023-03-18 02:15:13 +0900

    #63 [FEAT] 리뷰가 존재하는지 여부를 알려주는 entity 내 field 추가

--- src/main/java/com/developlife/reviewtwits/entity/Review.java
+++ src/main/java/com/developlife/reviewtwits/entity/Review.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Entity
 @Builder
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Review extends BaseEntity {
@@ -31,6 +32,9 @@ public class Review extends BaseEntity {
 
     @ColumnDefault(value = "false")
     private boolean certificationFlag;
+
+    @ColumnDefault(value = "true")
+    private boolean exist;
 
     private String content;
 
