cf33f6e9786d7ed965d626f5fb196fde804b8d62
WhalesBob <ndw8200@naver.com>
2023-03-20 20:55:07 +0900

    #79 [FEAT] 팔로우 기능에 연결된 controller 부분 추가

---
+++ src/main/java/com/developlife/reviewtwits/controller/SnsController.java
@@ -0,0 +1,32 @@
+package com.developlife.reviewtwits.controller;
+
+import com.developlife.reviewtwits.entity.User;
+import com.developlife.reviewtwits.message.request.sns.FollowRequest;
+import com.developlife.reviewtwits.service.SnsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+/**
+ * @author WhalesBob
+ * @since 2023-03-20
+ */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/sns")
+public class SnsController {
+
+    private final SnsService snsService;
+
+    @PostMapping("/requestFollow")
+    public void followProcess(@AuthenticationPrincipal User user, @Valid @RequestBody FollowRequest request){
+        snsService.followProcess(user, request.targetUserAccountId());
+    }
+}
