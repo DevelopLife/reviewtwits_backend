9e4c03db87d449b1f123b83a2d6efb5d9cf03cdc
WhalesBob <ndw8200@naver.com>
2023-03-21 16:35:29 +0900

    #81 [FEAT] 팔로워, 팔로잉 유저 리스트를 가지고 오는 JpaRepository method 추가

--- src/main/java/com/developlife/reviewtwits/repository/FollowRepository.java
+++ src/main/java/com/developlife/reviewtwits/repository/FollowRepository.java
@@ -3,12 +3,20 @@ package com.developlife.reviewtwits.repository;
 import com.developlife.reviewtwits.entity.Follow;
 import com.developlife.reviewtwits.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     Optional<Follow> findByUserAndTargetUser(User user, User targetUser);
     boolean existsByUserAndTargetUser(User user, User targetUser);
+
+    @Query("SELECT f.user FROM Follow f WHERE f.targetUser = :user")
+    List<User> findUsersByTargetUser(User user);
+
+    @Query("SELECT f.targetUser FROM Follow f WHERE f.user = :user")
+    List<User> findTargetUsersByUser(User user);
 }
