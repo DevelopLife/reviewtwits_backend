c4e1c066ede297bc1c30af7e85eacab8b47f55ef
marinelife <ghdic77@gmail.com>
2023-03-21 22:30:59 +0900

    #81 [FIX] 일부로 에러내보기

--- src/test/java/com/developlife/reviewtwits/user/UserApiTest.java
+++ src/test/java/com/developlife/reviewtwits/user/UserApiTest.java
@@ -56,7 +56,7 @@ public class UserApiTest extends ApiTest {
             .get("/users/search/{userId}")
         .then()
             .assertThat()
-            .statusCode(HttpStatus.OK.value())
+            .statusCode(HttpStatus.ALREADY_REPORTED.value())
             // 기본정보 표시
             .body("nickname", notNullValue())
             .body("accountId", notNullValue())
