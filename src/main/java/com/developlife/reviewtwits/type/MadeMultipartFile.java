81caa31c18798ac8098680fa051e668cd9d10854
WhalesBob <ndw8200@naver.com>
2023-03-23 14:55:29 +0900

    #94 [FEAT] 파일 byte[] 정보를 MultipartFile 정보로 변환하기 위해 Custom MultipartFile Type 추가

---
+++ src/main/java/com/developlife/reviewtwits/type/MadeMultipartFile.java
@@ -0,0 +1,61 @@
+package com.developlife.reviewtwits.type;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
+/**
+ * @author WhalesBob
+ * @since 2023-03-23
+ */
+public class MadeMultipartFile implements MultipartFile {
+
+    byte[] input;
+    String name;
+
+    public MadeMultipartFile(byte[] input, String name) {
+        this.input = input;
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getOriginalFilename() {
+        return this.name;
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return input == null || input.length == 0;
+    }
+
+    @Override
+    public long getSize() {
+        return input.length;
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        return input;
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return new ByteArrayInputStream(input);
+    }
+
+    @Override
+    public void transferTo(File destination) throws IOException, IllegalStateException {
+        FileUtils.writeByteArrayToFile(destination, input);
+    }
+}
