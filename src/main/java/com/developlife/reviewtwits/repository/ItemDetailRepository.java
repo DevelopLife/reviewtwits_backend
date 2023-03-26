66068217d94829021775c4fef904294c0bee4a5d
WhalesBob <ndw8200@naver.com>
2023-03-26 15:40:10 +0900

    #94 [FEAT] 대표 아이템 정보를 크롤링하기 위한 Entity 및 관련 Repository 세팅

---
+++ src/main/java/com/developlife/reviewtwits/repository/ItemDetailRepository.java
@@ -0,0 +1,7 @@
+package com.developlife.reviewtwits.repository;
+
+import com.developlife.reviewtwits.entity.ItemDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemDetailRepository extends JpaRepository<ItemDetail, Long> {
+}
