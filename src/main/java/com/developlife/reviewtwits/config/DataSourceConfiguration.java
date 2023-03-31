d225e87296cb4380bcf6aafc7d6d756334568739
marinelife <ghdic77@gmail.com>
2023-03-31 21:57:14 +0900

    #104 [FIX] @ConfigurationProperties 작동 해결

--- src/main/java/com/developlife/reviewtwits/config/DataSourceConfiguration.java
+++ src/main/java/com/developlife/reviewtwits/config/DataSourceConfiguration.java
@@ -1,9 +1,6 @@
 package com.developlife.reviewtwits.config;
 
-import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -11,6 +8,7 @@ import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -28,20 +26,12 @@ import java.util.HashMap;
  * @since 2023/03/29
  */
 @Configuration
+@Profile("!test")
 public class DataSourceConfiguration {
+
     private static final String SOURCE_SERVER = "SOURCE";
     private static final String REPLICA_SERVER = "REPLICA";
     private final JpaProperties jpaProperties;
-    @Value("${spring.datasource.source.url}")
-    private String masterUrl;
-    @Value("${spring.datasource.replica.url}")
-    private String slaveUrl;
-    @Value("${spring.datasource.driver-class-name}")
-    private String driverClassName;
-    @Value("${spring.datasource.username}")
-    private String username;
-    @Value("${spring.datasource.password}")
-    private String password;
 
     public DataSourceConfiguration(JpaProperties jpaProperties) {
         this.jpaProperties = jpaProperties;
@@ -51,38 +41,31 @@ public class DataSourceConfiguration {
     @Qualifier(SOURCE_SERVER)
     @ConfigurationProperties(prefix = "spring.datasource.source")
     public DataSource sourceDataSource() {
-        return createDataSource(masterUrl);
+        return DataSourceBuilder.create()
+            .build();
     }
 
     @Bean
     @Qualifier(REPLICA_SERVER)
     @ConfigurationProperties(prefix = "spring.datasource.replica")
     public DataSource replicaDataSource() {
-        return createDataSource(slaveUrl);
-    }
-
-    public DataSource createDataSource(String url) {
-        HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setJdbcUrl(url);
-        hikariDataSource.setDriverClassName(driverClassName);
-        hikariDataSource.setUsername(username);
-        hikariDataSource.setPassword(password);
-        return hikariDataSource;
+        return DataSourceBuilder.create()
+            .build();
     }
 
     @Bean
     public DataSource routingDataSource(
-        @Qualifier(SOURCE_SERVER) DataSource sourceDataSource,
+        @Qualifier(SOURCE_SERVER) DataSource sourceDataSource, // (1)
         @Qualifier(REPLICA_SERVER) DataSource replicaDataSource
     ) {
-        RoutingDataSource routingDataSource = new RoutingDataSource();
+        RoutingDataSource routingDataSource = new RoutingDataSource(); // (2)
 
-        HashMap<Object, Object> dataSourceMap = new HashMap<>();
+        HashMap<Object, Object> dataSourceMap = new HashMap<>(); // (3)
         dataSourceMap.put("source", sourceDataSource);
         dataSourceMap.put("replica", replicaDataSource);
 
-        routingDataSource.setTargetDataSources(dataSourceMap);
-        routingDataSource.setDefaultTargetDataSource(sourceDataSource);
+        routingDataSource.setTargetDataSources(dataSourceMap); // (4)
+        routingDataSource.setDefaultTargetDataSource(sourceDataSource); // (5)
 
         return routingDataSource;
     }
