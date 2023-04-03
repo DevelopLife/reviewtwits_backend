437f771deeccf88ffd9c4ad272167a7380ffbb95
WhalesBob <ndw8200@naver.com>
2023-04-03 18:21:55 +0900

    #122 [FEAT] 피드 Pagination 처리를 위한 JPA 기능 추가

--- src/main/java/com/developlife/reviewtwits/repository/ReviewRepository.java
+++ src/main/java/com/developlife/reviewtwits/repository/ReviewRepository.java
@@ -1,6 +1,8 @@
 package com.developlife.reviewtwits.repository;
 
 import com.developlife.reviewtwits.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,6 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findReviewsByProductUrl(String productURL);
+    Page<Review> findByReviewIdLessThan(long reviewId, Pageable pageable);
 }
 
