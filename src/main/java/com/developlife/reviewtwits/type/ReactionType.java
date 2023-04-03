89e191bea2cd60d9dfebe53edcefd4d02721cf13
WhalesBob <ndw8200@naver.com>
2023-04-03 18:25:33 +0900

    #122 [FEAT] 추가할 수 있는 리액션 타입 설정 및 리액션 관련 분류 기능 추가

---
+++ src/main/java/com/developlife/reviewtwits/type/ReactionType.java
@@ -0,0 +1,61 @@
+package com.developlife.reviewtwits.type;
+
+import com.developlife.reviewtwits.entity.Reaction;
+import com.developlife.reviewtwits.entity.User;
+import com.developlife.reviewtwits.message.response.review.ReactionResponse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public enum ReactionType {
+
+    LOVE("\uD83D\uDE0D"),
+    SUNGLASSES("\uD83D\uDE0E"),
+    LAUGHING("\uD83E\uDD23"),
+    SURPRISING("\uD83D\uDE32"),
+    THINKING("\uD83E\uDD14"),
+    PLEADING("\uD83E\uDD7A"),
+    CHOCKING("\uD83E\uDEE2"),
+    PRAYING("\uD83D\uDE4F"),
+    GOOD("\uD83D\uDC4D"),
+    NOTICING("\uD83D\uDC40");
+
+    private String emoji;
+
+    ReactionType(String emoji) {
+    }
+    
+    public static List<ReactionResponse> classifyReactionResponses(User user,List<Reaction> reactionList){
+        ReactionType userReactionType = null;
+        Map<ReactionType, Integer> reactionClassify = new HashMap<>();
+        for(Reaction reaction : reactionList){
+            try{
+                ReactionType reactionType = reaction.getReactionType();
+                Integer reactionCount = reactionClassify.get(reactionType);
+                reactionClassify.replace(reactionType, reactionCount + 1);
+            }catch(NullPointerException ex){
+                reactionClassify.put(reaction.getReactionType(), 1);
+            }
+
+            if(reaction.getUser().equals(user)){
+                userReactionType = reaction.getReactionType();
+            }
+        }
+        return mappingReactionResponse(userReactionType,reactionClassify);
+    }
+    private static List<ReactionResponse> mappingReactionResponse(
+            ReactionType userReactionType,Map<ReactionType, Integer> reactionClassify){
+
+        List<ReactionResponse> response = new ArrayList<>();
+        reactionClassify.forEach((reactionType, count) -> {
+            response.add(ReactionResponse.builder()
+                    .reactionType(reactionType)
+                    .count(count)
+                    .isReacted(reactionType == userReactionType)
+                    .build());
+        });
+        return response;
+    }
+}
