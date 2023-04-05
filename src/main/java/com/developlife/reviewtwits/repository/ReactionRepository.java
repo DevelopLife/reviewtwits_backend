3f81609f91c526ba3a834ec4e716d57c403a657e
WhalesBob <ndw8200@naver.com>
2023-04-05 19:25:20 +0900

    [FIX] @Repository 없는곳에 추가

--- src/main/java/com/developlife/reviewtwits/repository/ReactionRepository.java
+++ src/main/java/com/developlife/reviewtwits/repository/ReactionRepository.java
@@ -4,10 +4,12 @@ import com.developlife.reviewtwits.entity.Reaction;
 import com.developlife.reviewtwits.entity.Review;
 import com.developlife.reviewtwits.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ReactionRepository extends JpaRepository<Reaction, Long> {
     List<Reaction> findByReview(Review review);
     Optional<Reaction> findByReview_ReviewIdAndUser(long reviewId, User user);
