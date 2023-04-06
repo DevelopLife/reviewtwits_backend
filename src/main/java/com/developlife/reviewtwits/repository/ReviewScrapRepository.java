7ab712f428ec7ce4c2855e9ac854a00353a5cca9
WhalesBob <ndw8200@naver.com>
2023-04-07 01:45:20 +0900

    #132 [FEAT] 유저 정보로 ReviewScrap 가지고 오는 기능 추가

--- src/main/java/com/developlife/reviewtwits/repository/ReviewScrapRepository.java
+++ src/main/java/com/developlife/reviewtwits/repository/ReviewScrapRepository.java
@@ -4,9 +4,14 @@ import com.developlife.reviewtwits.entity.Review;
 import com.developlife.reviewtwits.entity.ReviewScrap;
 import com.developlife.reviewtwits.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReviewScrapRepository extends JpaRepository<ReviewScrap, Long> {
     Optional<ReviewScrap> findByReviewAndUser(Review review, User user);
+
+    @Query("SELECT rs.review FROM ReviewScrap rs WHERE rs.user = :user")
+    List<Review> findReviewByUser(User user);
 }
