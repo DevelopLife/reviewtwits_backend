617455ba454d0540904d88da16f2ea5055fa5150
WhalesBob <ndw8200@naver.com>
2023-04-07 23:27:56 +0900

    #132 [FIX] SnippetException 생기는 부분 바로잡기(userId 를 response 에 모두 추가)

--- src/main/java/com/developlife/reviewtwits/message/response/user/UserInfoResponse.java
+++ src/main/java/com/developlife/reviewtwits/message/response/user/UserInfoResponse.java
@@ -7,7 +7,7 @@ import lombok.Builder;
  * @since 2023/02/24
  */
 public record UserInfoResponse(
-        String userId,
+        long userId,
         String nickname,
         String accountId,
         String introduceText,
