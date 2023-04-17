0d4bcf663341c0f4b2dfa1274aa6d06cff378291
marinelife <ghdic77@gmail.com>
2023-04-17 22:41:47 +0900

    #158 [ADD] Redis 세팅

---
+++ src/main/java/com/developlife/reviewtwits/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package com.developlife.reviewtwits.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @author ghdic
+ * @since 2023/04/17
+ */
+@Configuration
+@ConfigurationProperties(prefix = "spring.redis")
+public class RedisConfig {
+    private String host;
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
