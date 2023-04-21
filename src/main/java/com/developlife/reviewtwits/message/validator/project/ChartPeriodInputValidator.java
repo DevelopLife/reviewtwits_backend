ecf44038f0150ecac6bb4c5cf6a6d66655aca502
WhalesBob <ndw8200@naver.com>
2023-04-21 16:16:14 +0900

    #157 [FEAT] 시간 범위 입력을 검증하는 ChartPeriodUnit Enum 기능 추가

---
+++ src/main/java/com/developlife/reviewtwits/message/validator/project/ChartPeriodInputValidator.java
@@ -0,0 +1,21 @@
+package com.developlife.reviewtwits.message.validator.project;
+
+import com.developlife.reviewtwits.message.annotation.project.ChartPeriod;
+import com.developlife.reviewtwits.type.ChartPeriodUnit;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * @author WhalesBob
+ * @since 2023-04-21
+ */
+public class ChartPeriodInputValidator implements ConstraintValidator<ChartPeriod, String> {
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if(value == null) {
+            return false;
+        }
+        return ChartPeriodUnit.checkChartPeriodInput(value);
+    }
+}
