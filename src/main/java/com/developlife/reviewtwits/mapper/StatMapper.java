055c434b9daf25a078c8f821a04415b6c6de33e5
WhalesBob <ndw8200@naver.com>
2023-04-23 15:14:17 +0900

    #157 [FEAT] 통계정보 저장 시 돌려줄 Response Message 및 Mapper 기능 추가

---
+++ src/main/java/com/developlife/reviewtwits/mapper/StatMapper.java
@@ -0,0 +1,32 @@
+package com.developlife.reviewtwits.mapper;
+
+import com.developlife.reviewtwits.entity.StatInfo;
+import com.developlife.reviewtwits.entity.User;
+import com.developlife.reviewtwits.message.response.statistics.SaveStatResponse;
+import com.developlife.reviewtwits.message.response.user.UserInfoResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy =  NullValuePropertyMappingStrategy.IGNORE, nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+public interface StatMapper {
+
+    @Mapping(target = "profileImageUrl", ignore = true)
+    @Mapping(target = "reviewCount", ignore = true)
+    @Mapping(target = "followers", ignore = true)
+    @Mapping(target = "followings", ignore = true)
+    UserInfoResponse mapUserToUserInfoResponse(User user);
+
+    default SaveStatResponse mapStatInfoToSaveStatResponse(StatInfo statInfo){
+        return SaveStatResponse.builder()
+                .userInfo(mapUserToUserInfoResponse(statInfo.getUser()))
+                .createdDate(statInfo.getCreatedDate().toLocalDate().toString())
+                .inflowUrl(statInfo.getInflowUrl())
+                .productUrl(statInfo.getProductUrl())
+                .productId(statInfo.getProduct().getProductId())
+                .projectId(statInfo.getProject().getProjectId())
+                .deviceInfo(statInfo.getDevice().toString())
+                .build();
+    }
+}
