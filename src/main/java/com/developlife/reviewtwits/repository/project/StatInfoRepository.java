5cce20a1f13edb7f6690f94b31b0a62072d80017
WhalesBob <ndw8200@naver.com>
2023-04-23 15:09:49 +0900

    #157 [CHORE] ProjectRepository, StatInfoRepository 를 project package 안으로 이동시킴

--- src/main/java/com/developlife/reviewtwits/repository/project/StatInfoRepository.java
+++ src/main/java/com/developlife/reviewtwits/repository/project/StatInfoRepository.java
@@ -1,4 +1,4 @@
-package com.developlife.reviewtwits.repository;
+package com.developlife.reviewtwits.repository.project;
 
 import com.developlife.reviewtwits.entity.StatInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
