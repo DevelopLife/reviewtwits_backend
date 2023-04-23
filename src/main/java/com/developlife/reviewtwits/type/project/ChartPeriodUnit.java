b7dc9b1271d536ac27355529d7a99dc768f1cb91
WhalesBob <ndw8200@naver.com>
2023-04-23 15:34:25 +0900

    #157 [FEAT] Repository 에서 사용 가능하도록 Enum 에서의 기능 추가

--- src/main/java/com/developlife/reviewtwits/type/project/ChartPeriodUnit.java
+++ src/main/java/com/developlife/reviewtwits/type/project/ChartPeriodUnit.java
@@ -1,23 +1,31 @@
 package com.developlife.reviewtwits.type.project;
 
+import com.querydsl.core.types.dsl.NumberExpression;
+
+import java.time.LocalDateTime;
+
+import static com.developlife.reviewtwits.entity.QStatInfo.statInfo;
+
 public enum ChartPeriodUnit {
 
-    ONE_DAY("1d"),
-    THREE_DAY("3d"),
-    FIVE_DAY("5d"),
-    SEVEN_DAY("7d"),
-    FIFTEEN_DAY("15d"),
-    ONE_MONTH("1mo"),
-    THREE_MONTH("3mo"),
-    SIX_MONTH("6mo"),
-    ONE_YEAR("1y"),
-    THREE_YEAR("3y"),
-    FIVE_YEAR("5y");
+    ONE_DAY("1d",LocalDateTime.now().minusDays(1)),
+    THREE_DAY("3d",LocalDateTime.now().minusDays(3)),
+    FIVE_DAY("5d",LocalDateTime.now().minusDays(5)),
+    SEVEN_DAY("7d",LocalDateTime.now().minusDays(7)),
+    FIFTEEN_DAY("15d",LocalDateTime.now().minusDays(15)),
+    ONE_MONTH("1mo",LocalDateTime.now().minusMonths(1)),
+    THREE_MONTH("3mo",LocalDateTime.now().minusMonths(3)),
+    SIX_MONTH("6mo",LocalDateTime.now().minusMonths(6)),
+    ONE_YEAR("1y",LocalDateTime.now().minusYears(1)),
+    THREE_YEAR("3y",LocalDateTime.now().minusYears(3)),
+    FIVE_YEAR("5y",LocalDateTime.now().minusYears(5));
 
     private final String inputValue;
+    private final LocalDateTime timeRangeBefore;
 
-    ChartPeriodUnit(String inputValue) {
+    ChartPeriodUnit(String inputValue, LocalDateTime timeRangeBefore) {
         this.inputValue = inputValue;
+        this.timeRangeBefore = timeRangeBefore;
     }
 
     public static boolean checkChartPeriodInput(String input) {
@@ -27,5 +35,51 @@ public enum ChartPeriodUnit {
             }
         }
         return false;
+    }
+
+    public static LocalDateTime getTimeRangeBefore(ChartPeriodUnit unit) {
+        return unit.timeRangeBefore;
+    }
+
+    public static NumberExpression<Integer> getExpressionOfInterval(ChartPeriodUnit interval){
+        String dayMonthYear = getDayMonthYear(interval);
+        int numberValue = getNumberValue(interval);
+
+        // dayMonthYear, numberValue 중 하나라도 -1 이면 에러를 배출해야 함.
+
+        if(dayMonthYear.equals("d")){
+            return statInfo.createdDate.dayOfYear().divide(numberValue);
+        }
+        if(dayMonthYear.equals("mo")){
+            return statInfo.createdDate.month().divide(numberValue);
+        }
+        if(dayMonthYear.equals("y")){
+            return statInfo.createdDate.year().divide(numberValue);
+        }
+
+        return null;
+    }
+
+    private static int getNumberValue(ChartPeriodUnit interval) {
+        String input = interval.inputValue;
+        if(input.endsWith("d") || input.endsWith("y")){
+            return Integer.parseInt(input.substring(0,input.length()-1));
+        }
+        if(input.endsWith("mo")){
+            return Integer.parseInt(input.substring(0,input.length()-2));
+        }
+        return -1;
+    }
+
+    private static String getDayMonthYear(ChartPeriodUnit unit){
+        String input = unit.inputValue;
+        if(input.endsWith("d") || input.endsWith("y")){
+            return input.substring(input.length()-1);
+        }
+        if(input.endsWith("mo")){
+            return input.substring(input.length()-2);
+        }
+
+        return "nothing";
     }
 }
