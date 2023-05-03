1550c8645e723b947cce23011e6f2190f8ffe587
WhalesBob <ndw8200@naver.com>
2023-05-04 02:45:28 +0900

    #203 [FEAT] 받아온 imageUrl 를 받아 이미지를 서버에 저장하고, Product 를 생성해 주는 로직 추가

--- src/main/java/com/developlife/reviewtwits/service/ProductService.java
+++ src/main/java/com/developlife/reviewtwits/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.developlife.reviewtwits.service;
 
+import com.developlife.reviewtwits.entity.FileInfo;
 import com.developlife.reviewtwits.entity.Product;
 import com.developlife.reviewtwits.entity.Project;
 import com.developlife.reviewtwits.entity.User;
@@ -9,6 +10,7 @@ import com.developlife.reviewtwits.message.request.product.ProductRegisterReques
 import com.developlife.reviewtwits.message.response.product.ProductRegisterResponse;
 import com.developlife.reviewtwits.repository.ProductRepository;
 import com.developlife.reviewtwits.repository.project.ProjectRepository;
+import com.developlife.reviewtwits.type.ReferenceType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +24,9 @@ public class ProductService {
 
     private final ProductRepository productRepository;
     private final ProjectRepository projectRepository;
+    private final FileStoreService fileStoreService;
+
+    private final String REQUEST_IMAGES = "/request-images/";
 
     public ProductRegisterResponse registerProductOnProject(User user, String projectName, ProductRegisterRequest request) {
 
@@ -32,15 +37,34 @@ public class ProductService {
             throw new AccessDeniedException("프로젝트에 대한 권한이 없습니다.");
         }
 
-        Product savedProduct = productRepository.save(Product.builder()
-                .project(project)
-                .productUrl(request.productUrl())
-                .build());
+        Product savedProduct = getSavedProduct(projectName, request, project);
 
         return ProductRegisterResponse.builder()
                 .productId(savedProduct.getProductId())
                 .projectId(savedProduct.getProject().getProjectId())
                 .productUrl(savedProduct.getProductUrl())
+                .productName(savedProduct.getProductName())
+                .productUrl(REQUEST_IMAGES + savedProduct.getProductUrl())
                 .build();
+    }
+
+    private Product getSavedProduct(String projectName, ProductRegisterRequest request, Project project) {
+        Product savedProduct = productRepository.save(Product.builder()
+                .project(project)
+                .productName(request.productName())
+                .productUrl(request.productUrl())
+                .build());
+
+        FileInfo savedImage = fileStoreService.downloadImageFileFromUrl(
+                request.imageUrl(),
+                projectName,
+                request.productName(),
+                savedProduct.getProductId(),
+                ReferenceType.PRODUCT
+        );
+
+        savedProduct.setImageUrl(savedImage.getFilePath());
+        productRepository.save(savedProduct);
+        return savedProduct;
     }
 }
