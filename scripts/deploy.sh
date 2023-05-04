af0c468e9654155527bcc40a5b9f9d0dbb99000b
marinelife <ghdic77@gmail.com>
2023-05-04 15:45:41 +0900

    [FIX] 배포시 kill되기 전에 실행되는부분을 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,8 +14,8 @@ if [ -z "$CURRENT_PID" ]; then
   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+  (sleep 1; kill -15 $CURRENT_PID) &
+  wait $CURRENT_PID
 
   echo "> nohup.out 삭제"
   rm nohup.out
