e9efc654fa7058cc4163b46901e5fb5bbc35057d
WhalesBob <ndw8200@naver.com>
2023-05-22 16:14:40 +0900

    #225 [REFACTOR] 리뷰 허가상태 요청 점검을 위한 ReviewApprove Target 확장

--- src/main/java/com/developlife/reviewtwits/message/annotation/review/ReviewApprove.java
+++ src/main/java/com/developlife/reviewtwits/message/annotation/review/ReviewApprove.java
@@ -3,12 +3,13 @@ package com.developlife.reviewtwits.message.annotation.review;
 import com.developlife.reviewtwits.message.validator.review.ReviewApproveValidator;
 
 import javax.validation.Constraint;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.FIELD)
+import static java.lang.annotation.ElementType.*;
+
+@Target({FIELD, PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = ReviewApproveValidator.class)
 public @interface ReviewApprove {
