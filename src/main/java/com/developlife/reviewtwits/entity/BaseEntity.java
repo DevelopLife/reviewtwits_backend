2c962977baa1cb2fbacfc8babb5775e731473934
WhalesBob <ndw8200@naver.com>
2023-06-06 01:48:36 +0900

    [CHORE] 사용하지 않는 import 삭제

--- src/main/java/com/developlife/reviewtwits/entity/BaseEntity.java
+++ src/main/java/com/developlife/reviewtwits/entity/BaseEntity.java
@@ -1,7 +1,5 @@
 package com.developlife.reviewtwits.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
