3b53821a4756cb58e96a0c75e7281b1e809d7c1d
Nam Dongwoo <96509257+WhalesBob@users.noreply.github.com>
2023-06-19 16:06:21 +0900

    #268 [REFACTOR] repository 전반에 있어 사용하고 있지 않는 import 모두 삭제

--- src/main/java/com/developlife/reviewtwits/repository/RefreshTokenRepository.java
+++ src/main/java/com/developlife/reviewtwits/repository/RefreshTokenRepository.java
@@ -4,7 +4,6 @@ import com.developlife.reviewtwits.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Ref;
 import java.util.Optional;
 
 /**
