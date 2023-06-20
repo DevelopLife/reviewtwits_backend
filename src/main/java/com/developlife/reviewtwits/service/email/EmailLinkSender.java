6f21834e2d72698afb489ae36f6791062101a755
marinelife <ghdic77@gmail.com>
2023-06-20 17:00:06 +0900

    #268 이메일sender에서 html코드 thymeleaf template engine으로 대체함

--- src/main/java/com/developlife/reviewtwits/service/email/EmailLinkSender.java
+++ src/main/java/com/developlife/reviewtwits/service/email/EmailLinkSender.java
@@ -5,6 +5,7 @@ import com.developlife.reviewtwits.type.EmailType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
@@ -19,30 +20,18 @@ import java.io.UnsupportedEncodingException;
 public class EmailLinkSender extends EmailSender {
 
     @Autowired
-    public EmailLinkSender(JavaMailSender emailSender, EmailVerifyRepository emailVerifyRepository) {
-        super(emailSender, emailVerifyRepository);
+    public EmailLinkSender(JavaMailSender emailSender, EmailVerifyRepository emailVerifyRepository, SpringTemplateEngine templateEngine) {
+        super(emailSender, emailVerifyRepository, templateEngine);
     }
 
-    protected MimeMessage createMessage(final String to, final String url) throws MessagingException, UnsupportedEncodingException {
+    protected MimeMessage createMessage(final String to, final String code) throws MessagingException, UnsupportedEncodingException {
         MimeMessage message = emailSender.createMimeMessage();
 
         message.addRecipients(MimeMessage.RecipientType.TO, to);//보내는 대상
         message.setSubject("Reviewtwits 비밀번호 초기화 메일입니다");//제목
 
-        String msgg="";
-        msgg+= "<div style='margin:20px;'>";
-        msgg+= "<h1> 안녕하세요 ReviewTwit의 비밀번호 초기화 메일입니다 </h1>";
-        msgg+= "<br>";
-        msgg+= "<p>아래 링크를 클릭 해주세요<p>";
-        msgg+= "<br>";
-        msgg+= "<p>감사합니다.<p>";
-        msgg+= "<br>";
-        msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
-        msgg+= "<h3 style='color:blue;'>회원가입 링크입니다.</h3>";
-        msgg+= "<div style='font-size:130%'>";
-        msgg+= "링크 : <strong><a href='http://localhost:3000/reset-password?code=%s'>비밀번호 초기화하기</a></strong><div><br/>".formatted(url);
-        msgg+= "</div>";
-        message.setText(msgg, "utf-8", "html");//내용
+
+        message.setText(setContext("resetPassword", code), "utf-8", "html");//내용
         message.setFrom(new InternetAddress("ghdic77@gmail.com","reviewtwit"));//보내는 사람
 
         return message;
