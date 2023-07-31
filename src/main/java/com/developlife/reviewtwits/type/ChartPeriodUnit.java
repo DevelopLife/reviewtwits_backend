cbb675e335a6a853979a505b5a1b065f534b9d89
Nam Dongwoo <96509257+WhalesBob@users.noreply.github.com>
2023-07-31 20:28:18 +0900

    #273 [REFACTOR] 사용하지 않는 단위 enum 삭제

--- src/main/java/com/developlife/reviewtwits/type/ChartPeriodUnit.java
+++ src/main/java/com/developlife/reviewtwits/type/ChartPeriodUnit.java
@@ -6,17 +6,10 @@ import java.time.LocalDateTime;
 public enum ChartPeriodUnit {
 
     ONE_DAY("1d"),
-    THREE_DAY("3d"),
-    FIVE_DAY("5d"),
-    SEVEN_DAY("7d"),
-    FIFTEEN_DAY("15d"),
+    ONE_WEEK("1w"),
     ONE_MONTH("1mo"),
-    THREE_MONTH("3mo"),
-    SIX_MONTH("6mo"),
     ONE_YEAR("1y"),
-    THREE_YEAR("3y"),
     FIVE_YEAR("5y");
-
     private final String inputValue;
 
     ChartPeriodUnit(String inputValue) {
